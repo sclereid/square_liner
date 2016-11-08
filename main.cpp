a396bb0ecdf2b6aab58d99e66865a949ca62c7ef
virtualizerocket <1307689960@qq.com>
2016-11-08 22:19:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,26 +11,26 @@
 
 using namespace std;
 
-int k, c, tc1, tc2;
-double tx, ty, maxz, minz;
-double cm, cn;
-double m[105], n[105], p[105];
-double x[10005], y[10005], z[10005];
+int k, c, tc1, tc2; //k: number the of equations, c: number the of avalible points, tc1&tc2:adress to the max and min values
+double tx, ty, maxz, minz; //temporary values
+double cm, cn; //expection equation parameters
+double m[105], n[105], p[105]; // limit equation parameters
+double x[10005], y[10005], z[10005]; //avalible points and its value
 
 int liner2_solve(double a, double b, double c, double d, double e, double f)
 {
     double tmp = a*e - b*d;
     if(tmp == 0)
-        return 1;
+        return 1; //parallel equations
     tx = (b*f - c*e)/tmp;
     ty = (c*d - a*f)/tmp;
     return 0;
-}
+} //solve two equtions
 
 bool judge(double a, double b, double c)
 {
     return (tx*a+tx*b-c)<=0;
-}
+} //judge if it is in range
 
 
 int main()
@@ -55,11 +55,11 @@ int main()
     {
         cout << "Can't solve.\n";
     }
-    cout << "\nz = ax + by + c:\n";
+    cout << "\nz = ax + by:\n";
     cin >> cm >> cn;
     maxz = -21747483648;
     minz = 21747483647;
-    bool good_value;
+    bool good_value;//good value is a point in range
     for (int i = 0; i < c; i++)
     {
         good_value = true;
@@ -81,10 +81,11 @@ int main()
             tc2 = i;
         }
     }
-    
+    //fliter and choose maxium and minium number
     cout << "\n\n";
     cout << "max{z} = " << maxz << " where x = " << x[tc1] << " and y = " << y[tc1] << '\n';
     cout << "min{z} = " << minz << " where x = " << x[tc2] << " and y = " << y[tc2] << '\n';
     cout << endl;
+    //output
     return 0;
 }
